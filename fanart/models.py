2b38c382da6bd9f19a3a8babe254c046d4d4bc91
taekyu32-lee <taekyu32@gmail.com>
2022-12-06 23:18:01 +0900

    Feat: image 저장 방법 수정 #4
    - 이미지 저장 할 때, model에서 파일명을 uuid로 바꿔 저장

--- fanart/models.py
+++ fanart/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from user.models import User
 from webtoon.models import Webtoon
+import os
+from uuid import uuid4
+
+# image uuid로 저장
+class UploadTo:
+    def __init__(self, folder = None):
+        self.folder = folder
+    def save_path(self, instance, filename):
+        uuid = f"{uuid4().hex}.png"
+        return f"fanart/{self.folder}/{uuid}"
 
 class BaseImage(models.Model):
     image = models.ImageField(upload_to='fanart/resize/') # 채색 제거, 크기 조정된 이미지
 
 class FanartImage(models.Model):
     resize_image = models.ForeignKey(BaseImage, on_delete=models.CASCADE)
-    hint_image = models.ImageField(upload_to='fanart/hint/') # resize_image에 채색 힌트가 추가된 이미지
+    hint_image = models.ImageField(upload_to=UploadTo(folder='hint').save_path) # resize_image에 채색 힌트가 추가된 이미지
     result_image = models.ImageField(upload_to='fanart/result/') # 채색 완료된 이미지
 
 class Fanart(models.Model):
