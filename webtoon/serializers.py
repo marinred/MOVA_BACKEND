d2bf572d2d43118ce2f636eb1b567eb9172518e0
seminju0172@gmail.com <seminju0172@gmail.com>
2022-12-06 18:02:34 +0900

    Fix: WebtoonDetailVeiwSerializer fix  #6
    - WebtoonDetailVeiwSerializer add webtoon_likes_count
    - WebtoonDetailVeiwSerializer add webtoon_bookmarks_count

--- webtoon/serializers.py
+++ webtoon/serializers.py
@@ -12,7 +12,14 @@ class WebtoonViewSerializer(serializers.ModelSerializer):
         fields = ('id', 'platform', 'author', 'title', 'genre', 'image_url', 'day_of_the_week', 'likes_count',)
         
 class WebtoonDetailVeiwSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    bookmarks_count = serializers.SerializerMethodField()
+    
+    def get_likes_count(self, obj):
+        return obj.webtoon_likes.count()
+    def get_bookmarks_count(self, obj):
+        return obj.webtoon_bookmarks.count()
     
     class Meta:
         model = Webtoon
-        fields = '__all__'
+        fields = ('id', 'platform', 'title', 'author', 'image_url', 'summary', 'genre', 'day_of_the_week', 'webtoon_link', 'likes_count', 'bookmarks_count',)
