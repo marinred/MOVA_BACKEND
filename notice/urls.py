6c01bb2e6ae574f26189bc051e09e6b3aad8ee10
marinred <endrms1995@gmail.com>
2022-12-06 20:12:08 +0900

    Feat: Notice_urls.py,admin.py  #2
    - 프로젝트, 앱 urls.py 설정
    - admin.py 설정

--- notice/urls.py
+++ notice/urls.py
@@ -4,4 +4,5 @@ from notice import views
 
 urlpatterns = [
     path('', views.NoticeView.as_view(), name='notice_view'),
+    path('<int:notice_id>/', views.NoticeDetailView.as_view(), name='notice_detail_view'),
 ]
