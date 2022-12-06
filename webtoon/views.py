0e37b408f4704d15e148ad2b3cde17dcbebb8eeb
seminju0172@gmail.com <seminju0172@gmail.com>
2022-12-06 17:14:57 +0900

    Feat: add WebtoonDetailVeiw #6
    - add WebtoonDetailVeiw

--- webtoon/views.py
+++ webtoon/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from webtoon.models import Webtoon
-from webtoon.serializers import WebtoonViewSerializer
+from webtoon.serializers import WebtoonViewSerializer, WebtoonDetailVeiwSerializer
 import datetime
 
 class WebtoonView(APIView):
@@ -12,3 +12,9 @@ class WebtoonView(APIView):
         webtoon = Webtoon.objects.filter(day_of_the_week=days[a])
         webtoon_serializer = WebtoonViewSerializer(webtoon, many=True)
         return Response(webtoon_serializer.data, status=status.HTTP_200_OK)
+    
+class WebtoonDetailVeiw(APIView):
+    def get(self, request, webtoon_id):
+        webtoon_detail = Webtoon.objects.get(id=webtoon_id)
+        webtoon_detail_serializer = WebtoonDetailVeiwSerializer(webtoon_detail)
+        return Response(webtoon_detail_serializer.data, status=status.HTTP_200_OK)
