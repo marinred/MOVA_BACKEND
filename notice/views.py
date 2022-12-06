135c98c86abb2133c0e8606501862a151c4a2694
marinred <endrms1995@gmail.com>
2022-12-06 20:16:00 +0900

    Feat: Notice_게시글 상세페이지 PUT 구현  #2
    - 게시글 상세페이지 PUT 기능 구현

--- notice/views.py
+++ notice/views.py
@@ -25,4 +25,14 @@ class NoticeDetailView(APIView):
     def get(self, request, notice_id):
         notice = Notice.objects.get(id=notice_id)
         serializer = NoticeSerializer(notice)
-        return Response(serializer.data, status=status.HTTP_200_OK)      
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, notice_id):
+        notice = Notice.objects.get(id=notice_id)
+        if request.user == notice.user:
+            serializer = NoticeSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)     
