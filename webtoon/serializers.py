1f02ebc8807e5743b616147320d1ceccf520fa19
seminju0172@gmail.com <seminju0172@gmail.com>
2022-12-06 17:01:16 +0900

    Feat: WebtoonViewSerializer add likes_count #6
    - WebtoonViewSerializer add webtoon_likes_count

--- webtoon/serializers.py
+++ webtoon/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from webtoon.models import Webtoon
 
 class WebtoonViewSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    
+    def get_likes_count(self, obj):
+        return obj.webtoon_likes.count()
     
     class Meta:
         model = Webtoon
-        fields = ('id', 'platform', 'title', 'image_url', 'day_of_the_week',)
+        fields = ('id', 'platform', 'title', 'image_url', 'day_of_the_week', 'likes_count',)
