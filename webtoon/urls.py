940b9af4b87f90762b9f218b5bee3935ba30c4f5
seminju0172@gmail.com <seminju0172@gmail.com>
2022-12-06 17:03:10 +0900

    Feat: add webtoon_detail_view urls #6
    - add webtoon_detail_view urls

--- webtoon/urls.py
+++ webtoon/urls.py
@@ -3,4 +3,5 @@ from webtoon import views
 
 urlpatterns = [
     path('', views.WebtoonView.as_view(), name='webtoon_view'),
+    path('<int:webtoon_id>/', views.WebtoonDetailVeiwSerializer.as_view(), name='webtoon_detail_view'),
 ]
