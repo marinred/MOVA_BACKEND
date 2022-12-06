c53bfdd17ce0fd2c02ddb2bceb1b0008a1b185bb
chorimgenius <artyum15@kakao.com>
2022-12-06 14:10:41 +0900

    Feat: webtoon 모델 생성 #6
    - webtoon 모델 생성

--- webtoon/models.py
+++ webtoon/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Webtoon(models.Model):
+    platform = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    image_url = models.CharField(max_length=60)
+    summary = models.TextField()
+    genre = models.CharField(max_length=20)
+    day_of_the_week = models.CharField(max_length=10)
+    webtoon_link = models.CharField(max_length=100)
+    webtoon_bookmarks = models.ManyToManyField(User)
+    webtoon_likes= models.ManyToManyField(User)
