c1b476588d57ef876cb20ee31e304b150397c99d
taekyu32-lee <taekyu32@gmail.com>
2022-12-06 23:10:01 +0900

    Feat: 채색을 위한 colorization 정의 #4
    - fanart/colorization.py def colorization 함수 추가
    
    Close #이슈번호

--- fanart/colorization.py
+++ fanart/colorization.py
@@ -4,6 +4,7 @@ from .utils import models
 import numpy as np
 import torch
 from PIL import Image
+from uuid import uuid4
 
 def sketchProcess(image_name):
 
@@ -45,3 +46,47 @@ def sketchProcess(image_name):
     img_.save(img_media + img_dir_)
 
     return img_dir_
+
+def colorization(resize_id,hint_id):
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    with torch.no_grad():
+        netC2S = models.Color2Sketch(input_nc=3,
+                                        output_nc=1,
+                                        norm='IN',
+                                        SN=True,
+                                        activation='relu',
+                                        residual=False,
+                                        ckpt_path='fanart/utils/checkpoint/color2sketch.pth').to(device)
+        netS2C = models.Colorizenet(input_nc=4,
+                                    output_nc=3,
+                                    norm='IN',
+                                    SN=True,
+                                    activation='relu',
+                                    residual=True,
+                                    ckpt_path='fanart/utils/checkpoint/sketch2color.pth').to(device)
+
+        netC2S.eval()
+        netS2C.eval()
+
+    # Iamge process
+    hint = cv2.imread(hint_id, cv2.IMREAD_COLOR) # img = cv2.resize(img, dsize=(512, 512), interpolation=cv2.INTER_AREA)
+    hint = cv2.cvtColor(hint, cv2.COLOR_BGR2RGB)
+    hint = cv2.resize(hint, dsize=(512,512),interpolation=cv2.INTER_AREA)
+
+
+    resize = cv2.imread(resize_id, cv2.IMREAD_GRAYSCALE).reshape(512, 512, 1)
+    input_array = np.concatenate([resize, hint], axis=2)
+    with torch.no_grad():
+        result = process.ForwardNet(input_array, netS2C, device)
+    result = result * 255
+    result = result.astype(np.uint8)
+
+    # Save result
+    uuid = uuid4().hex +'.png'
+    img_media = 'media/'
+    result_dir = "fanart/result/"+uuid
+    result = Image.fromarray(result)
+    result.save(img_media+result_dir)
+
+    return result_dir
