7eeab24cef759ea754b0f6543404f4d44190078d
seminju0172@gmail.com <seminju0172@gmail.com>
2022-12-06 18:21:44 +0900

    Feat: retouch webtoon views.py #6
    - Add remark
    - Add likes webtoon view

--- webtoon/views.py
+++ webtoon/views.py
@@ -6,20 +6,34 @@ import datetime
 from webtoon.models import Webtoon
 from webtoon.serializers import WebtoonViewSerializer, WebtoonDetailVeiwSerializer
 
+# Webtoon Mainpage
 class WebtoonView(APIView):
     def get(self, request):
+        
+        # Today Webtoon View
         days=['월', '화', '수', '목', '금', '토', '일']
         a = datetime.datetime.today().weekday()
-        webtoon = Webtoon.objects.filter(day_of_the_week=days[a])
-        webtoon_serializer = WebtoonViewSerializer(webtoon, many=True)
-        return Response(webtoon_serializer.data, status=status.HTTP_200_OK)
-    
+        today_webtoon = Webtoon.objects.filter(day_of_the_week=days[a])
+        today_webtoon_serializer = WebtoonViewSerializer(today_webtoon, many=True)
+        
+        # Likes Webtoon View
+        user = self.request.user
+        bookmarks_webtoon = user.user_bookmarks.all()
+        bookmarks_webtoon_serializer = WebtoonViewSerializer(bookmarks_webtoon, many=True)
+        
+        response_data = []
+        response_data.append(today_webtoon_serializer.data)
+        response_data.append(bookmarks_webtoon_serializer.data)
+        return Response(response_data, status=status.HTTP_200_OK)
+
+# Webtoon Detailpage
 class WebtoonDetailVeiw(APIView):
     def get(self, request, webtoon_id):
         webtoon_detail = Webtoon.objects.get(id=webtoon_id)
         webtoon_detail_serializer = WebtoonDetailVeiwSerializer(webtoon_detail)
         return Response(webtoon_detail_serializer.data, status=status.HTTP_200_OK)
-    
+
+# Webtoon Like
 class WebtoonLikeView(APIView):
     def post(self, request, webtoon_id):
         webtoon = get_object_or_404(Webtoon, id=webtoon_id)
@@ -30,7 +44,8 @@ class WebtoonLikeView(APIView):
         else:
             webtoon.webtoon_likes.add(request.user)
             return Response("좋아요 등록 완료!", status=status.HTTP_200_OK)
-        
+
+# Webtoon Bookmark
 class WebtoonBookmarkView(APIView):
     def post(self, request, webtoon_id):
         webtoon = get_object_or_404(Webtoon, id=webtoon_id)
