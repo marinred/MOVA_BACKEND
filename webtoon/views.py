4e6116c2650a471f04dc2ffc9c9d40a20820ed78
seminju0172@gmail.com <seminju0172@gmail.com>
2022-12-06 16:06:58 +0900

    Feat: webtoon views.py setting #6
    - WebtoonView add

--- webtoon/views.py
+++ webtoon/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from webtoon.models import Webtoon
+from webtoon.serializers import WebtoonViewSerializer
+import datetime
 
-# Create your views here.
+class WebtoonView(APIView):
+    def get(self, request):
+        days=['월', '화', '수', '목', '금', '토', '일']
+        a = datetime.datetime.today().weekday()
+        webtoon = Webtoon.objects.filter(day_of_the_week=days[a])
+        webtoon_serializer = WebtoonViewSerializer(webtoon, many=True)
+        return Response(webtoon_serializer.data, status=status.HTTP_200_OK)
