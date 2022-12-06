0bf10610d392f2582bb2a5bcde725a286b89e98c
marinred <endrms1995@gmail.com>
2022-12-06 20:13:40 +0900

    Feat: NoticeSerializer username 추가  #2
    - Serializer 내 username 추가

--- notice/serializers.py
+++ notice/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from notice.models import Notice
 
 class NoticeSerializer(serializers.ModelSerializer):
+    notice_user = serializers.SerializerMethodField()
+    
+    def get_notice_user(self, obj):
+        return obj.user.username
     
     class Meta:
         model = Notice
