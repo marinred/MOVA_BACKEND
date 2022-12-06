91630f6433d02500f570847c7ad5df18981595db
marinred <endrms1995@gmail.com>
2022-12-06 20:40:27 +0900

    Docs: Settings.py import 항목 수정
    - settings.py timedelta import 항목 누락사항 수정

--- mova/settings.py
+++ mova/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
