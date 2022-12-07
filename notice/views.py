cc5903cf6f71832e68f180dea8deeb9894a7a2c4
marinred <endrms1995@gmail.com>
2022-12-07 17:26:00 +0900

    Fix: POST시 body에 user 포함 제외 및 결과값에서는 user 나오게 하는 기능 구현 #14
    - serializer.py 수정
    - views.py 수정(serializer.save에 user=request.user, 시리얼라이저 변경)

--- notice/views.py
+++ notice/views.py
@@ -15,9 +15,9 @@ class NoticeView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        serializer = NoticeSerializer(data=request.data)
+        serializer = NoticeCreateSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)       
