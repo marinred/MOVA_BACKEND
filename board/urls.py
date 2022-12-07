6e0191d0b35850532b4ba3faa7836444affac959
marinred <endrms1995@gmail.com>
2022-12-07 15:09:22 +0900

    Feat: Board_Detail_PUT&DELETE url 설정  #17
    - BoardDetail_PUT&DELETE url 설정

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,5 @@ from board import views
 
 urlpatterns = [
     path('', views.BoardView.as_view(), name='board_view'),
+    path('<int:board_id>/', views.BoardDetailView.as_view(), name='board_detail_view'),
 ]
