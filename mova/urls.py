dba1a1cb1a7d3c0208e13758396f36e6b622df89
jinyeobjeong <yeobdol@gmail.com>
2022-12-07 17:08:53 +0900

    Feat: user profile 기능 구현 #1
    - profile 기능구현을 위한 urls.py 수정

--- mova/urls.py
+++ mova/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
+    path("account/", include("django.contrib.auth.urls")),
 ] 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
