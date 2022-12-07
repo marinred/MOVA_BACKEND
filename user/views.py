3308a4a2600542dcb684558e4e57fa1b035bc277
jinyeobjeong <yeobdol@gmail.com>
2022-12-07 17:57:53 +0900

    Feat : 회원 탈퇴 기능 추가 #1
    - user앱의 views.py 에서 delete 기능 추가

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from user.models import User
-from user.serializers import UserSerializer, CustomObtainPairSerializer,  UserProfileSerializers, UserProfileUpdateSerializers
+from user.serializers import UserSerializer, CustomObtainPairSerializer,  UserProfileSerializers, UserProfileUpdateSerializers, UserDeleteSerializers
 from django.shortcuts import get_object_or_404
 #from webtoon.serializers import WebtoonSerializer
 
@@ -16,14 +16,6 @@ class UserView(APIView):
         else:
             return Response({"message" : f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
         
-    # def delete(self, request, **kwargs): #회원탈퇴
-    #     if kwargs.get('user_id') is None:
-    #         return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
-    #     else:
-    #         user_id = kwargs.get('user_id')
-    #         user_object = User.objects.get(id=user_id)
-    #         user_object.delete()
-    #         return Response("test ok", status=status.HTTP_200_OK)
             
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomObtainPairSerializer
@@ -36,13 +28,21 @@ class ProfileView(APIView):
         serializer_user = UserProfileSerializers(user)
         return Response(serializer_user.data, status=status.HTTP_200_OK)
     
-#프로필수정
+#프로필 수정
     def put(self, request):
-            user = get_object_or_404(User, id=request.user.id)
-            serializer = UserProfileUpdateSerializers(user, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user = get_object_or_404(User, id=request.user.id)
+        serializer = UserProfileUpdateSerializers(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+#회원탈퇴
+    def delete(self, request):
+        user = get_object_or_404(User, id=request.user.id)
+        if user.username == request.data["username"]:
+            user.delete()
+            return Response("성공")
+        else:
+            return Response("실패")
