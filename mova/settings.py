316dcbcee664c25bea8c44aea1b9f00e2f0687c4
jinyeobjeong <yeobdol@gmail.com>
2022-12-08 15:40:28 +0900

    Feat : cors 추가
    - settings.py cors 추가
    
    Close #이슈번호

--- mova/settings.py
+++ mova/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
     "user"
@@ -80,6 +80,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -88,6 +89,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "mova.urls"
 
