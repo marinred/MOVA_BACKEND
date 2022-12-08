85401af48f09d91bfa38133f4c1258266017bba9
marinred <endrms1995@gmail.com>
2022-12-08 14:49:01 +0900

    Feat: 게시판 상세페이지 댓글 포함 시리얼라이저 추가 및 수정   #17
    - 게시판 상세페이지 댓글 포함 시리얼라이저 추가 및 수정 완료

--- board/serializers.py
+++ board/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from board.models import Board
+from board.models import BoardComment
 
 
 class BoardSerializer(serializers.ModelSerializer):
@@ -12,11 +13,21 @@ class BoardSerializer(serializers.ModelSerializer):
         model = Board
         fields = '__all__'
 
+class BoardCommentSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+    
+    def get_username(self, obj):
+        return obj.user.username  
+    class Meta:
+        model = BoardComment
+        fields = ('id', 'username', 'board', 'comment', 'created_at' , 'updated_at',)
+
 class BoardDetailSerializer(serializers.ModelSerializer):
     board_user = serializers.SerializerMethodField()
+    board_comment_set = BoardCommentSerializer(many=True)
     
     def get_board_user(self, obj):
         return obj.user.username
     class Meta:
         model = Board
-        fields = ("title", "content", "category","image", "webtoon","board_user",)
+        fields = ("title", "content", "category","image", "webtoon","board_user","board_comment_set",)
