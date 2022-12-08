cc9f3bdd36998a036d22e1b5d4718a70a2e0c130
marinred <endrms1995@gmail.com>
2022-12-08 14:46:08 +0900

    Feat: BoardCommnet GET url 설정  #17
    - 게시판 상세페이지 댓글 GET url 설정

--- board/urls.py
+++ board/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.BoardView.as_view(), name='board_view'),
     path('<int:board_id>/', views.BoardDetailView.as_view(), name='board_detail_view'),
+    path('<int:board_id>/comment/', views.BoardCommentView.as_view(), name='board_comment_view'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
