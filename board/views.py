f571fdd3a5ffcdbd952381056e30b56cd6416617
marinred <endrms1995@gmail.com>
2022-12-08 15:18:39 +0900

    Feat: 게시글 상세페이지 댓글 PUT 기능 구현  #17
    - 게시글 상세페이지 댓글 url 설정
    - 게시글 상세페이지 댓글 PUT 기능 구현

--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from board.models import Board
+from board.models import BoardComment
 from board.serializers import BoardSerializer
 from board.serializers import BoardDetailSerializer
 from board.serializers import BoardCommentSerializer
@@ -63,3 +64,17 @@ class BoardCommentView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+        
+class BoardCommentDetailView(APIView):
+    def put(self, request, board_id, boardcomment_id):
+        comment = BoardComment.objects.get(id=boardcomment_id)
+        if request.user == comment.user:
+            serializer = BoardCommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
+        else:
+            return Response("권한이 존재하지 않습니다.", status=status.HTTP_403_FORBIDDEN)
+            
