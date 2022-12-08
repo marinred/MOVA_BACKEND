966e1126a7281de1c692320e3b3d791e5e339f2f
marinred <endrms1995@gmail.com>
2022-12-09 01:12:01 +0900

    Feat: Notice_Category 모델링  #2
    - Notice models.py 모델링 구현

--- notice/models.py
+++ notice/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from user.models import User
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return (self.category_name)
+    
+
+
 class Notice(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.CharField(max_length=30)
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='notice_category_set')
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
