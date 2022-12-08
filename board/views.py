2b41574f18536fdfcdc444770fd3c29ecb2bef52
marinred <endrms1995@gmail.com>
2022-12-08 14:50:03 +0900

    Feat: BoardCommentView GET 기능 구현  #17
    - 게시글 상세페이지 댓글 포함 GET 수정
    - 댓글 GET 기능 구현

--- board/views.py
+++ board/views.py
@@ -4,11 +4,12 @@ from rest_framework.response import Response
 from board.models import Board
 from board.serializers import BoardSerializer
 from board.serializers import BoardDetailSerializer
+from board.serializers import BoardCommentSerializer
+from board.serializers import BoardCommentCreateSerializer
 
 # Create your views here.
 class BoardView(APIView):
     def get(self, request):
-        pass
         board = Board.objects.all()
         serializer = BoardSerializer(board, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -24,7 +25,7 @@ class BoardView(APIView):
 class BoardDetailView(APIView):
     def get(self, request, board_id):
         board = Board.objects.get(id=board_id)
-        serializer = BoardSerializer(board)
+        serializer = BoardDetailSerializer(board)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, board_id):
@@ -47,4 +48,11 @@ class BoardDetailView(APIView):
         else:
             return Response("권한이 존재하지 않습니다.", status=status.HTTP_403_FORBIDDEN)
         
+
+class BoardCommentView(APIView):
+    def get(self, request, board_id):
+        board = Board.objects.get(id=board_id)
+        comments = board.board_comment_set.all()
+        serializer = BoardCommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
         
