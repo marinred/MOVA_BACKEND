28146fe6ee00cb207a52b7467de703c90c478589
marinred <endrms1995@gmail.com>
2022-12-13 17:44:12 +0900

    Fix: pagination 수정, serializer #2
    - notice pagination.py page_size 수정
    - serialzier/ notice_category_name SerializerMethodField 추가

--- notice/pagination.py
+++ notice/pagination.py
@@ -2,4 +2,4 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class NoticePagination(PageNumberPagination):
-    page_size = 10
+    page_size = 30
--- notice/serializers.py
+++ notice/serializers.py
@@ -4,6 +4,7 @@ from notice.models import Notice
 class NoticeSerializer(serializers.ModelSerializer):
     notice_user = serializers.SerializerMethodField()
     notice_user_profile_image = serializers.SerializerMethodField()
+    notice_category_name = serializers.SerializerMethodField()
     
     def get_notice_user(self, obj):
         return obj.user.username
@@ -11,8 +12,9 @@ class NoticeSerializer(serializers.ModelSerializer):
     def get_notice_user_profile_image(self, obj):
         return obj.user.image.url
     
+    def get_notice_category_name(self, obj):
+        return obj.category_name.category_name
+    
     class Meta:
         model = Notice
-        fields = ("notice_user","notice_user_profile_image","title","content","created_at","updated_at","category_name")
-    
-    
+        fields = ("id","notice_user","notice_user_profile_image","title","content","created_at","updated_at","category_name", "notice_category_name")
