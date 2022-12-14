fea72f6d1a93ba1d7e91551d6b2118c6cb026e11
taekyu32-lee <taekyu32@gmail.com>
2022-12-14 12:39:33 +0900

    Feat: fanart board 검색기능 #4
    - 검색했을 때 일치하는 게시글만 리턴

--- fanart/urls.py
+++ fanart/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("baseimage/", views.BaseImageView.as_view()),
     path("colorization/", views.ColorizationView.as_view()),
     path("", views.FanartListView.as_view()),
+    path("search/<int:webtoon_id>/",views.FanartWebtoonListView.as_view()),
     path("<int:fanart_id>/", views.FanartView.as_view()),
     path("<int:fanart_id>/comment/", views.FanartCommentView.as_view()),
     path("<int:fanart_id>/comment/<int:comment_id>", views.FanartCommentView.as_view())
