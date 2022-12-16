8534b9432eb526955507955458d6a57d976e69a8
marinred <endrms1995@gmail.com>
2022-12-16 19:37:26 +0900

    Feat: 배포를 위한 settings.py 추가
    - 배포 진행을 위한 settings.py 추가
    - migrations 초기화

--- mova/settings.py
+++ mova/settings.py
@@ -7,6 +7,10 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
