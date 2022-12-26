a72a9e5e7471aa3f44fbe98dae9d9639589a6a9a
chorimgenius <artyum15@kakao.com>
2022-12-26 14:57:39 +0000

    Feat: postgresql 연동을 위한 Modelfields 수정 #76
    - postgresql 연동을 위해 webtoon model charfield 제한 변경(255)

--- webtoon/models.py
+++ webtoon/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from user.models import User
 
 class Webtoon(models.Model):
-    platform = models.CharField(max_length=20)
-    title = models.CharField(max_length=20)
-    author = models.CharField(max_length=30)
-    image_url = models.CharField(max_length=60)
+    platform = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=255)
     summary = models.TextField()
-    genre = models.CharField(max_length=20)
-    day_of_the_week = models.CharField(max_length=10)
-    webtoon_link = models.CharField(max_length=100)
+    genre = models.CharField(max_length=255)
+    day_of_the_week = models.CharField(max_length=255)
+    webtoon_link = models.CharField(max_length=255)
     webtoon_bookmarks = models.ManyToManyField(User, related_name="webtoon_bookmarks_set")
     webtoon_likes= models.ManyToManyField(User, related_name="webtoon_likes_set")
     
